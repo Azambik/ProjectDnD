7f6468367b372eb68b1768447ca435fe55ca4fa3
Azambik <andrewjzambik@yahoo.com>
2023-02-28 17:48:41 -0500

    basic landing page set up.

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { User } from './models/User';
 import { HttpService } from './services/http.service';
 import { PrimeNGConfig } from 'primeng/api';
+import { Subscription } from 'rxjs';
+//import { ModalService } from '../app/services/modal-service/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +12,38 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit{
 
-  constructor(private httpService: HttpService, private primeConfig: PrimeNGConfig) {}
+  constructor(
+    private httpService: HttpService, 
+    private primeConfig: PrimeNGConfig
+    /*private modalService: ModalService*/) {}
+
+   /* @ViewChild('modal', { read: ViewContainerRef })
+    entry!: ViewContainerRef;
+    sub!: Subscription;*/
+  
 
   users?: User[] = [];
 
   ngOnInit(): void {
     this.primeConfig.ripple = true;
 
-    this.httpService.getAllUsers().toPromise().then((users: User[] | undefined) => {
+    /*this.httpService.getAllUsers().toPromise().then((users: User[] | undefined) => {
       this.users = users;
-    }).catch((err) => console.error(err));
+    }).catch((err) => console.error(err));*/
   }
+
+  /*openModal() {
+    this.sub = this.modalService
+      .openModal(this.entry, 'Are you sure ?', 'click confirm or close')
+      .subscribe((v) => {
+        //your logic
+      });
+    }
+    ngOnDestroy(): void {
+      if (this.sub) this.sub.unsubscribe();
+    }*/
+    test():void{
+      console.log("test");
+    }
+
 }
